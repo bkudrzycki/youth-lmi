257fb0e1da3e36d187fbf5a9c5d3472ad7ffae3c
bkudrzycki <bkudrzycki@gmail.com>
2021-04-07 16:42:17 +0200

    new root data format

--- lamadex/R/compute_indicators.R
+++ lamadex/R/compute_indicators.R
@@ -12,16 +12,16 @@ compute_indicators <- function() {
   # dfList[c(1:4,6,8,9)] <- map(dfList[c(1:4,6,8,9)], ~filter(.x, obs_status.label != "Unreliable"))
 
   ## reload indicators into dataframe, calculating indicators from raw data as needed
-  load("data/neet.rda")
+  load("./data/neet.rda")
 
   ## calculate relative working poverty rate by dividing the unemployment rate for youth by that of all workers over 25 years of age
-  load("data/working_pov.rda")
+  load("./data/working_pov.rda")
   relative_workpov <- working_pov %>%
     pivot_wider(names_from = classif1.label, values_from = obs_value) %>%
     mutate(obs_value = `Age (Youth, adults): 15-24`/`Age (Youth, adults): 25+`)
 
   ## calculate relative underemployment rate by dividing the unemployment rate for youth by that of all workers over 25 years of age
-  load("data/underemp.rda")
+  load("./data/underemp.rda")
   relative_underemp <- underemp %>%
     pivot_wider(names_from = classif1.label, values_from = obs_value) %>%
     mutate(obs_value = `Age (Youth, adults): 15-24`/`Age (Youth, adults): 25+`)
@@ -31,12 +31,12 @@ compute_indicators <- function() {
     mutate(obs_value = (obs_value.x + obs_value.y)/2)
 
   ## calculate mismatch rate by summing over the differences in the youth employment and unemployment rates by (ILO aggregate) education level
-  load("data/employed.rda")
+  load("./data/employed.rda")
   employed <- employed %>%
     dplyr::select(ref_area.label, classif2.label, sex.label, time, obs_value) %>%
     pivot_wider(names_from = c(classif2.label), values_from = obs_value)
 
-  load("data/unemployed.rda")
+  load("./data/unemployed.rda")
   unemployed <- unemployed %>%
     dplyr::select(ref_area.label, classif2.label, sex.label, time, obs_value) %>%
     pivot_wider(names_from = c(classif2.label), values_from = obs_value)
@@ -55,7 +55,7 @@ compute_indicators <- function() {
   underemp <- underemp %>%
     filter(classif1.label == "Age (Youth, adults): 15-24")
 
-  load("data/informal.rda")
+  load("./data/informal.rda")
   informal <- informal %>%
     pivot_longer(cols = c("Sex: Total","Sex: Male","Sex: Female"),
                  names_to = "sex.label",
@@ -64,7 +64,7 @@ compute_indicators <- function() {
 
 
   ## calculate elementary work rate - rate of youth working in elementary occupations
-  load("data/occupation.rda")
+  load("./data/occupation.rda")
   elementary <- occupation %>%
     filter(classif1.label %in% c("Age (Youth bands): 15-19", "Age (Youth bands): 20-24"),
            classif2.label %in% c("Occupation (ISCO-08): 9. Elementary occupations",
@@ -73,7 +73,7 @@ compute_indicators <- function() {
     mutate(obs_value = 100 * rowSums(.[c(11,13)]) / rowSums(.[c(10,12)]))
 
   ## calculate rate of youth with no secondary schooling
-  load("data/education.rda")
+  load("./data/education.rda")
   nosecondary <- education %>%
     mutate("Sex: Female" = rowSums(.[c(3:5)]),
          "Sex: Male" = rowSums(.[c(12:14)]),
@@ -84,9 +84,9 @@ compute_indicators <- function() {
                  values_to = "obs_value",
                  names_ptypes = list(sex.label = factor(levels = c("Sex: Total","Sex: Male","Sex: Female")))) ## pivot to longer form
 
-  load("data/literacy.rda")
+  load("./data/literacy.rda")
 
-  load("data/test_scores.rda")
+  load("./data/test_scores.rda")
 
   dfList <- list(neet, relative_wc, mismatch, working_pov, underemp, informal, elementary, nosecondary, literacy, test_scores)
 
