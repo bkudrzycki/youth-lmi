f48d360255961a39d406f5dddf9d50a2672349e2
bkudrzycki <bkudrzycki@gmail.com>
2021-04-07 16:47:09 +0200

    remove webtool_test

--- webtool/webtool_test.R
+++
@@ -1,7 +0,0 @@
-# install.packages("shiny")
-library(shiny)
-runGitHub("youth-lmi", "bkudrzycki", subdir = "webtool", launch.browser = TRUE)
-
-
-
-#runGitHub("webtool_test", "bkudrzycki", launch.browser = TRUE)
